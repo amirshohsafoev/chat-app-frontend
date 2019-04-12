881d01bec56fdfe6ab7c3863ac8f97042f209605
amirshohsafoev <amirsafoev0@gmail.com>
2019-04-12 18:00:47 -0400

    first commit frontend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ActionCable from "action-cable-react-jwt";
 
 class App extends Component {
+  // componentDidMount() {
+  //   this.createSocket();
+  // }
+  //
+  // createSocket = () => {
+  //   // get your JWT token
+  //   // this is an example using localStorage
+  //   const yourToken = "1";
+  //   let App = {};
+  //   App.cable = ActionCable.createConsumer(
+  //     "ws://localhost:3000/cable",
+  //     yourToken
+  //   );
+  //   const subscription = App.cable.subscriptions.create(
+  //     { channel: "ChatroomsChannel", message_id: "3" },
+  //     {
+  //       connected: () => {},
+  //       disconnected: () => {},
+  //       received: data => {
+  //         console.log(App);
+  //       }
+  //     }
+  //   );
+  // };
+  //
+  // componentWillUnmount() {
+  //   this.subscription &&
+  //     this.context.cable.subscriptions.remove(this.subscription);
+  // }
+
+  // componentDidMount() {
+  //   fetch();
+  // }
+
   render() {
     return (
       <div className="App">
---
+++ src/Containers/LoginForm.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+class Login extends React.Component {}
+export default Login;
